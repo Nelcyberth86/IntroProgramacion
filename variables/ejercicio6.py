fa1082dffdcd14773bba3365fbc76161771ebe03
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-09-29 18:16:21 -0400

    clase 29/2020

---
+++ variables/ejercicio6.py
@@ -0,0 +1,5 @@
+peso_en_kg =int(input("ingrese, su_peso_en_kg_por_favor: "))
+estatura_en_m= float(input("ingrese, su-estatura_en_metros_por_favor: "))
+var1 = peso_en_kg / estatura_en_m
+frase = "Tu_índice_de_masa_corporal_es: " + str(var1)
+print(frase)
