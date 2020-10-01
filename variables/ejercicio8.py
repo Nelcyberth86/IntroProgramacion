bc44844ebe1f2ea2c68cee32ee93a156ab6d36df
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-01 19:42:03 -0400

    ganancia de invertir plata

---
+++ variables/ejercicio8.py
@@ -0,0 +1,8 @@
+monto = int(input("ingrese, monto : "))
+interes = int(input("ingrese, el interes : "))
+años = int(input("ingrese, el numero de años : "))
+
+ganancia_neta = print(int(monto) *int(1 + interes) ** int(años) )
+print(ganancia_neta)
+
+
