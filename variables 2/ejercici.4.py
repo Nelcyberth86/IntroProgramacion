23e4e8ba86377033e8b1439b9c789051ee34ea4f
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-01 18:05:45 -0400

    01/10 variables ejer.4

---
+++ variables 2/ejercici.4.py
@@ -0,0 +1,13 @@
+num1 = int(input("ingrese, numero 1: "))
+num2 = int(input("ingrese numero 2: "))
+son_iguales = num1 == num2
+print(f"num1 es igual a num2: {son_iguales}")
+
+distinto= num1 != num2
+print(f"num1 es ditinto a num2 : {distinto}")
+
+num1_mayor_num2= num1>num2
+print(f"num1 es mayor que num2: {num1_mayor_num2}")
+
+num1_menor_num2 = num1<=num2
+print(f"num1 es menor o igual que num2: {num1_menor_num2}")
