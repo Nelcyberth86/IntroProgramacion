900062bd88deb4c702c19275a9460f686141f30f
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-01 18:08:49 -0400

    01/10 variables cambio sin f

--- variables 2/ejercicio.1.py
+++ variables 2/ejercicio.1.py
@@ -1,4 +1,3 @@
 num1_millas = float(input("ingrese, numero en millas: "))
 num2_km = (num1_millas * 1.60934)
-mensaje =f"{num1_millas} equivale a {num2_km} en kilometros"
-print(mensaje)
+print(num1_millas,  "equivale a", num2_km, "en kilometros" )
