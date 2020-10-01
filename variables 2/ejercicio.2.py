15cb9d4f0a4a016693c74514562bc5a8d865a2d2
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-01 17:22:58 -0400

    01/10 variables ejer.2

---
+++ variables 2/ejercicio.2.py
@@ -0,0 +1,5 @@
+dividendo = int(input("ingresa, numero dividendo: "))
+divisor= int(input("ingresa; numero divisor: "))
+resto =( dividendo/divisor )
+mensaje = f"El resto obtenido al dividir {dividendo} entre {divisor} es {round(resto),2} "
+print(mensaje)
