5ec33b3777fa90b4b968dac7232c1afdc10aaf1f
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-06 11:54:17 -0400

    trbajo practico flujo condicinal

---
+++ flujo_condicional/ejercicio4.py
@@ -0,0 +1,12 @@
+monto_de_pago = int(input("ingrese, consumo de agua m3: "))
+if monto_de_pago < int(100) :
+    monto= int(monto_de_pago * 1)
+    print(f"usted tiene que pagar {monto}")
+elif monto_de_pago > 100 and monto_de_pago > 499:
+    monto2= float(monto_de_pago * 1.20)
+elif monto_de_pago > 500 and monto_de_pago > 999:
+    monto3= float(monto_de_pago * 1.50)
+    print(f"usted tiene que cancelar {monto3}")
+elif monto_de_pago > 1000:
+    monto4= int(monto_de_pago * 2)
+    print(f"usted tiene que cancelar {monto4}")
---
+++ flujo_condicional/ejercicio5.py
@@ -0,0 +1,8 @@
+num1= int(input("escribir numero; "))
+if num1 == 0 :
+    print("es cero")
+elif num1 < 0 :
+    print("es negativo")
+else:
+    num1 > 0
+    print("es positivo")
