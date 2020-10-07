d3c5dc08a6caf825f5a2af28f9b6eec0f3361d31
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-07 19:25:17 -0400

    flujo ciclico

---
+++ flujo_ciclico/ejercicio5.py
@@ -0,0 +1,8 @@
+#E5: Escribir un programa que lea un número por teclado y muestre una triángulo de altura equivalente al número leído.
+num = int(input("ingrese,un numero porfavor : "))
+aumentar_un_por_cada_fila= 1 * num
+for num in range(1,num,) :
+    num1 = "*" * num
+    print(num1)
+
+
