a780723c416936650b21489967fdddbe0c587fa5
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-10-14 11:01:13 -0400

    15

--- practico_1/ejercicio_15.1.py
+++ practico_1/ejercicio_15.1.py
@@ -1,7 +1,6 @@
 #desarrolle un programa que grafique los largos de la secuencia de collatz de los numeros enteros positivos ,menores ingresado por el usuario
 n = int(input("ingrese un numero entero: "))
 valor = int(float(n))
-asterisco= "*"
 while valor > 1 :
     if valor % 2 == 0:
         valor = valor / 2
