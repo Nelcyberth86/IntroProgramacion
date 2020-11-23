d07f201f3bf56684a196b513fe8ac99f78cc39a2
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-11-23 15:22:30 -0400

    ejercicios

---
+++ practico_vectores/ejercicio1.py
@@ -0,0 +1,12 @@
+#Diseñar un programa que almacene en  un vector llamado FACT, el factorial de los primeros 20 números naturales.
+# FACT = {1!, 2!, 3!, … 20!} . Debe diseñar una función que calcule el factorial de un número, por lo tanto,
+#esta función tiene un parámetro y DEVUELVE (o retorna) un valor.
+def factorial(n):
+    FACT = []
+    factorial = 1
+    for i in range(1,n):
+        factorial = factorial * i
+        FACT.append(factorial)
+    print(FACT)
+numero=int(input("Ingrese un numero por favor: "))
+num=factorial(numero)
---
+++ practico_vectores/ejercicio2.py
@@ -0,0 +1,14 @@
+#Diseñar un programa que lea como entrada dos vectores de tamaño 5 y devuelva el vector suma.
+#Ejemplo: si tenemos los vectores V1 = (a1, a2, …, a5) y V2 = (b1, b2, …, b5) el vector suma se define como el vector obtenido
+#de sumar componente a componente: V1 + V2 = (a1+ b1, a2+ b2, …, a5+ b5).
+#import array as np
+num1=input("ingrese un caracter:")
+num2=input("ingrese un caracter:")
+v1 = [num1]*5
+v2 = [num2]*5
+suma = []
+
+for i in range(len(v1)):
+    suma.append(v1[i] + v2[i])
+
+print(suma)
