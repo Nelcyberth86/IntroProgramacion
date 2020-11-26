913c653eea0b9a3425c78c311064e6fc2c53d290
Nelcyberth86 <mishel.maraz2003@gmail.com>
2020-11-26 08:28:12 -0400

    practico vectores

--- practico_vectores/ejercicio1.py
+++ practico_vectores/ejercicio1.py
@@ -2,13 +2,14 @@
 # FACT = {1!, 2!, 3!, … 20!} . Debe diseñar una función que calcule el factorial de un número, por lo tanto,
 #esta función tiene un parámetro y DEVUELVE (o retorna) un valor.
 def factorial(n):
+    print("para",n)
     FACT = []
     factorial = 1
-    for i in range(1,n):
+    for i in range(1,n+1):
         factorial = factorial * i
         FACT.append(factorial)
+
     print(FACT)
+for n in range(0,20):
+    factorial(n)
 
-numero=int(input("Ingrese un numero por favor: "))
-
-num=factorial(numero)
--- practico_vectores/ejercicio2.py
+++ practico_vectores/ejercicio2.py
@@ -2,15 +2,28 @@
 #Ejemplo: si tenemos los vectores V1 = (a1, a2, …, a5) y V2 = (b1, b2, …, b5) el vector suma se define como el vector obtenido
 #de sumar componente a componente: V1 + V2 = (a1+ b1, a2+ b2, …, a5+ b5).
 #import array as np
-num1=input("ingrese un caracter:")
-num2=input("ingrese un caracter:")
 
-
-v1 = [num1]*5
-v2 = [num2]*5
+v1 = []
+v2 = []
 suma = []
 
-for i in range(len(v1)):
-    suma.append(v1[i] + v2[i])
+print("llene el primer vector")
+for i in range(0,5):
+    num1 = int(input("ingrese un caracter: "))
+    v1.append(num1)
 
+print("llene el segundo vector")
+for i in range(0, 5):
+    num2 = int(input("ingrese un caracter:"))
+    v2.append(num2)
+suma=[]
+print("suma de los vectores")
+for i in range(0,5):
+    num=v1[i]+v2[i]
+    suma.append(num)
+print(v1)
+print(v2)
 print(suma)
+
+
+
